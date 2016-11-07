8056a1dbebbf79eb8a283df116256d7b917c7c16
sidorov <sidorov@majordomo.ru>
2016-11-07 15:27:34 +0300

    another typo

--- src/taskexecutor/reporter.py
+++ src/taskexecutor/reporter.py
@@ -68,8 +68,8 @@ class AMQPReporter(Reporter):
         self._exchange = "{0}.{1}".format(self._task.res_type,
                                           self._task.action)
         self._routing_key = self._task.params["provider"]
-        LOGGER("Publishing to {0} exchange with "
-               "{1} routing key".format(self._exchange, self._routing_key))
+        LOGGER.info("Publishing to {0} exchange with "
+                    "{1} routing key".format(self._exchange, self._routing_key))
         self._connection = self._connnect()
         self._channel = self._open_channel()
         self._declare_exchange(self._exchange, CONFIG.amqp.exchange_type)
