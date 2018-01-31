77cd5959b2eb26ef92e911f09149482bc3979949
sidorov <sidorov@majordomo.ru>
2018-01-31 15:25:44 +0300

    account tarnsfer + cms install

--- src/taskexecutor/resdataprocessor.py
+++ src/taskexecutor/resdataprocessor.py
@@ -1,8 +1,11 @@
 import abc
 import docker
+import os
+import shutil
 
 from taskexecutor.config import CONFIG
 import taskexecutor.utils
+import taskexecutor.baseservice
 
 __all__ = ["Builder"]
 
@@ -11,7 +14,7 @@ class BuilderTypeError(Exception):
     pass
 
 
-class UnsupportedDstUriScheme(Exception):
+class PostprocessorArgumentError(Exception):
     pass
 
 
@@ -67,17 +70,53 @@ class StringReplaceDataProcessor(DataPostprocessor):
         file_globs = self.args.get("fileNameGlobs") or self.default_file_globs
         search_pattern = self.args.get("searchPattern")
         replace_string = self.args.get("replaceString")
-        find_expr = "( {} )".format(" -or ".join(file_globs)).translate(str.maketrans(self.shell_escape_map))
+        find_expr = "( {} )".format(" -or ".join(["-name {}".format(g)
+                                                  for g in file_globs])).translate(str.maketrans(self.shell_escape_map))
         cmd = ("find -O3 {0} {1} -type f "
                "-exec grep -q -e'{2}' {{}} \; -and "
                "-exec sed -i 's/{2}/{3}/g' {{}} \;").format(cwd, find_expr, search_pattern, replace_string)
         taskexecutor.utils.exec_command(cmd)
 
 
+class DataEraser(DataPostprocessor):
+    @property
+    def supported_data_types(self):
+        return ["directory", "database"]
+
+    def _erase_directory(self):
+        path = self.args.get("path") or self.args.get("cwd")
+        if not path:
+            raise PostprocessorArgumentError("No directory path was specified")
+        uid = os.stat(path).st_uid
+        shutil.rmtree(path)
+        os.mkdir(path)
+        os.chown(path, uid, uid)
+
+    def _erase_database(self):
+        name = self.args.get("name")
+        if not name:
+            raise PostprocessorArgumentError("No database name was specified")
+        db_server = self.args.get("dbServer")
+        if not db_server:
+            raise PostprocessorArgumentError("No database server was specified")
+        if not isinstance(db_server, taskexecutor.baseservice.DatabaseServer):
+            raise PostprocessorArgumentError("{} is not a database server".format(db_server))
+        db_server.drop_database(name)
+        db_server.create_database(name)
+
+    def process(self):
+        type = self.args.get("dataType")
+        if type not in self.supported_data_types:
+            raise PostprocessorArgumentError("{} data type is not supported by {}".format(type,
+                                                                                          self.__class__.__name__))
+        getattr(self, "_erase_{}".format(type))()
+
+
 class Builder:
     def __new__(cls, postproc_type):
         DataPostprocessorClass = {"docker": DockerDataPostprocessor,
-                                  "string-replace": StringReplaceDataProcessor}.get(postproc_type)
+                                  "string-replace": StringReplaceDataProcessor,
+                                  "eraser": DataEraser}.get(postproc_type)
         if not DataPostprocessorClass:
             raise BuilderTypeError("Unknown data postprocessor type: {}".format(postproc_type))
         return DataPostprocessorClass
