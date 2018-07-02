f8c5a3b8c8948cae27d8ecee110fdd47a60eba37
sidorov <sidorov@majordomo.ru>
2018-07-02 16:24:56 +0300

    support for upload/delete directories by ftp (for archiving purposes)

--- src/python/taskexecutor/ftpclient.py
+++ src/python/taskexecutor/ftpclient.py
@@ -1,5 +1,6 @@
 import io
 import ftplib
+import os
 
 
 class FTPClient:
@@ -24,16 +25,41 @@ class FTPClient:
         except ftplib.error_temp:
             self._connect()
 
+
+    def _check_dir(self, dirname):
+        filelist = []
+        self._server.retrlines('LIST', filelist.append)
+        found = False
+        for f in filelist:
+            if f.split()[-1] == dirname and f.lower().startswith('d'):
+                found = True
+        if not found:
+            self._server.mkd(dirname)
+        self._server.cwd(dirname)
+
+
     def upload(self, file, remote_filename):
         self._test_connection()
+        dirpath, filename = os.path.split(remote_filename)
+        for d in dirpath.split('/'):
+            if d != '':
+                self._check_dir(d)
         if not isinstance(file, io.BufferedIOBase):
             file = open(file, "rb")
         with file as f:
-            self._server.storbinary("STOR {}".format(remote_filename), f)
+            self._server.storbinary("STOR {}".format(filename), f)
+        self._server.cwd("/")
 
     def delete(self, remote_filename):
         self._test_connection()
+        dirpath, filename = os.path.split(remote_filename)
         try:
-            self._server.delete(remote_filename)
+            self._server.cwd(dirpath)
+            self._server.delete(filename)
+            dirs = [d for d in dirpath.split('/') if d != '']
+            while dirs:
+                self._server.cwd("..")
+                self._server.rmd(dirs.pop())
         except ftplib.error_perm:
             pass
+        self._server.cwd("/")
