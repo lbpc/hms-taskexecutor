6d070c60d44463d398ca276b8f9946e4978ffe56
sidorov <sidorov@majordomo.ru>
2018-07-03 17:40:42 +0300

    nice tar & gzip

--- src/python/taskexecutor/opservice.py
+++ src/python/taskexecutor/opservice.py
@@ -415,7 +415,7 @@ class MySQL(taskexecutor.baseservice.DatabaseServer, SysVService):
     def get_archive_stream(self, source, params={}):
         stdout, stderr = taskexecutor.utils.exec_command(
                 "mysqldump -h{0.address} -P{0.port} "
-                "-u{1.user} -p{1.password} {2} | gzip -9c".format(self.socket.mysql, CONFIG.mysql, source),
+                "-u{1.user} -p{1.password} {2} | nice -n 19 gzip -9c".format(self.socket.mysql, CONFIG.mysql, source),
                 return_raw_streams=True
         )
         return stdout, stderr
