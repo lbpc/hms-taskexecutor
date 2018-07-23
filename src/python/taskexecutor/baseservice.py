b749b8543d8ee289d67e560c67b25df6ae75cfed
Артём Моносов <monosov@majordomo.ru>
2018-07-23 13:07:36 +0000

    fix archive

--- src/python/taskexecutor/baseservice.py
+++ src/python/taskexecutor/baseservice.py
@@ -240,7 +240,7 @@ class ApplicationServer(ArchivableService):
 
     def get_archive_stream(self, source, params={}):
         stdout, stderr = taskexecutor.utils.exec_command("nice -n 19 "
-                                                         "tar czf --ignore-failed-read - -C {0} {1}".format(params.get("basedir"), source),
+                                                         "tar --ignore-failed-read -czf - -C {0} {1}".format(params.get("basedir"), source),
                                                          return_raw_streams=True)
         return stdout, stderr
 
