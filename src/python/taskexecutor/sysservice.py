ad4d6593ce2b16fbd6f51c96e06c79b6927e717b
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-03-11 20:54:35 +0000

    Please, don't delete hosting_accounts!

--- src/python/taskexecutor/sysservice.py
+++ src/python/taskexecutor/sysservice.py
@@ -104,7 +104,8 @@ class LinuxUserManager(UnixAccountManager):
         return "/usr/sbin/nologin"
 
     def create_group(self, name, gid=None):
-        taskexecutor.utils.exec_command("groupdel {} || true".format(name))
+        if not 'hosting_accounts' in name:
+            taskexecutor.utils.exec_command("groupdel {} || true".format(name))
         setgid = "--gid {}".format(gid) if gid else ""
         taskexecutor.utils.exec_command("groupadd --force {0} {1}".format(setgid, name))
 
