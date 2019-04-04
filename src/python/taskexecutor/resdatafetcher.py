d28b7429dc8556e0d613463a4db4851675bda3f8
Петр Сидоров <sidorov@majordomo.ru>
2019-04-04 14:28:53 +0000

    Update resdatafetcher.py

--- src/python/taskexecutor/resdatafetcher.py
+++ src/python/taskexecutor/resdatafetcher.py
@@ -113,7 +113,7 @@ class RsyncDataFetcher(DataFetcher):
         self.restic_repo = None
         self.dst_path = urllib.parse.urlparse(dst_uri).path
         if self.dst_host.split(":")[0] in CONFIG.backup.server.names and \
-                self.dst_path.split('/')[1:2] == CONFIG.backup.server.restic_location:
+                self.dst_path.split('/')[1:2] == [CONFIG.backup.server.restic_location]:
             self.restic_repo = self.dst_path.split('/')[2:3]
 
     def _mount_restic_repo(self):
