174b505074a881dae27b5f9a825a7962ae5dd101
sidorov <sidorov@majordomo.ru>
2019-04-24 17:24:55 +0300

    fix `=` in config values

--- src/python/taskexecutor/opservice.py
+++ src/python/taskexecutor/opservice.py
@@ -315,7 +315,8 @@ class MySQL(taskexecutor.baseservice.DatabaseServer, SysVService):
             if mysqld_section_started and line.startswith("["):
                 break
             if mysqld_section_started and line and not line.startswith("#") and "=" in line:
-                variable, value = line.split("=")
+                variable, *value = line.split("=")
+                value = "=".join(value)
                 if "-" not in variable:
                     config_vars[variable.strip()] = value.strip()
         actual_vars = {row[0]: row[1] for row in self.dbclient.execute_query("SHOW VARIABLES", ())}
