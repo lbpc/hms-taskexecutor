7ebd83ccb21922a38ea41f08bf173b5b7cfdd2b6
sidorov <sidorov@majordomo.ru>
2020-06-03 17:04:57 +0300

    do not stop already stopped switched off services

--- src/taskexecutor/resprocessor.py
+++ src/taskexecutor/resprocessor.py
@@ -487,12 +487,13 @@ class ServiceProcessor(ResProcessor):
             each.render_template(service=self.service, params=self.params)
             each.write()
         try:
-            if self.resource.switchedOn and self.service.status() is ServiceStatus.UP:
+            status = self.service.status()
+            if self.resource.switchedOn and status is ServiceStatus.UP:
                 self.service.reload()
             elif self.resource.switchedOn:
                 LOGGER.warning(f'{self.service.name} is down, starting it')
                 self.service.start()
-            else:
+            elif status is ServiceStatus.UP:
                 self.service.stop()
         except:
             for each in configs: each.revert()
