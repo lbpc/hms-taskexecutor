ba0173f99f7b931e1aac8206777133f182d88cb0
Oleg Pykhalov <go.wigust@gmail.com>
2021-03-30 14:50:14 +0300

    flake: packages: te: Use pkgs from input.

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
+    nixpkgs-19-09 = {
+      url = "github:NixOS/nixpkgs/ce9f1aaa39ee2a5b76a9c9580c859a74de65ead5";
+      flake = false;
+    };
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
@@ -10,7 +14,7 @@
     majordomo.url = "git+https://gitlab.intr/_ci/nixpkgs";
   };
 
-  outputs = { self, majordomo, nixpkgs-unstable, ... }:
+  outputs = { self, majordomo, nixpkgs-19-09, nixpkgs-unstable, ... }:
     let
       pkgs-unstable = import nixpkgs-unstable { inherit system; };
       system = "x86_64-linux";
@@ -21,11 +25,15 @@
       devShell.${system} = pkgs-unstable.mkShell {
         buildInputs = [ pkgs-unstable.nixUnstable ];
       };
-      packages.${system} = {
-        te = pkgs.callPackage ./te.nix {};
-        container = pkgs.callPackage ./default.nix {};
-        deploy = majordomo.outputs.deploy { tag = "hms/taskexecutor"; };
-      };
+      packages.${system} =
+        let
+          pkgs = import nixpkgs-19-09 { inherit system; };
+        in {
+          te = pkgs.callPackage ./te.nix { inherit pkgs; };
+        } // {
+          container = pkgs.callPackage ./default.nix { inherit pkgs; };
+          deploy = majordomo.outputs.deploy { tag = "hms/taskexecutor"; };
+        };
       defaultPackage.${system} = self.packages.${system}.container;
     };
 }
